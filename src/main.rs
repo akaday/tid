903ff01678393d3ba7ce4e9644f435b51f2620d6
aka <akadev8@outlook.com>
2025-01-05 10:07:56 +0100

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use image::GenericImageView;
+
+fn main() {
+    let img = image::open("path/to/your/image.png").unwrap();
+    let (width, height) = img.dimensions();
+
+    for y in 0..height {
+        for x in 0..width {
+            let pixel = img.get_pixel(x, y);
+            let brightness = (0.3 * pixel[0] as f32 + 0.59 * pixel[1] as f32 + 0.11 * pixel[2] as f32) / 255.0;
+            let ascii = match brightness {
+                0.0..=0.2 => ' ',
+                0.2..=0.4 => '.',
+                0.4..=0.6 => '*',
+                0.6..=0.8 => 'O',
+                _ => '#',
+            };
+            print!("{}", ascii);
+        }
+        println!();
+    }
+}
